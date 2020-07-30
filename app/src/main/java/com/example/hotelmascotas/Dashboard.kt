bd7f4ac6edfd11832a0d8a71de10d180a27ecfa6
Matías <palominos90@gmail.com>
2020-07-30 01:38:08 -0400

    Se añade fragment home y fragment menu

--- app/src/main/java/com/example/hotelmascotas/Dashboard.kt
+++ app/src/main/java/com/example/hotelmascotas/Dashboard.kt
@@ -2,11 +2,37 @@ package com.example.hotelmascotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hotelmascotas.fragments.HomeFragment
+import com.example.hotelmascotas.fragments.MenuFragment
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class Dashboard : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val homeFragment = HomeFragment()
+        val menuFragment = MenuFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigationHome -> makeCurrentFragment(homeFragment)
+                R.id.navigationMenu -> makeCurrentFragment(menuFragment)
+            }
+            true
+        }
     }
+
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
+
 }
